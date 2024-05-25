bf0a1fa56ccc4fd0c7cb8b6742dd85b562bd6ca2
Sourin Mukherjee <surajdey9830@gmail.com>
2024-05-25 11:23:49 +0530

    JSP update
    paypal
    
    Co-Authored-By: Sourin Mukherjee <98275372+i-am-faith@users.noreply.github.com>

--- gg.java
+++ gg.java
@@ -3,6 +3,10 @@ class gg
     public static void main(String[] args) {
         {
             System.out.println("Starting the JSP Server");
+            for(int i=0;i<5;i++)
+            {
+                System.out.print("Number = " + i + " \n");
+            }
         }
     }
 }
